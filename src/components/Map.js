d3afbbd41255b10a203bf417ed80e76ab96710e7
amad-person <e0072565@u.nus.edu>
2019-12-20 12:17:33 +0530

    Add Map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,33 @@
 import React from "react";
+import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
 import "./Map.css";
 
-const map = props => {
-  return (
-    <div className="map">
-      Latitude: {props.coords.latitude}
-      <br />
-      Longitude: {props.coords.longitude}
-    </div>
-  );
-};
+class Map extends React.Component {
+  render() {
+    const userLocation = [
+      this.props.coords.latitude,
+      this.props.coords.longitude
+    ];
+    return (
+      <LeafletMap
+        center={userLocation}
+        zoom={15}
+        maxZoom={25}
+        attributionControl={false}
+        zoomControl={true}
+        doubleClickZoom={true}
+        scrollWheelZoom={true}
+        dragging={true}
+        animate={true}
+        easeLinearity={0.35}
+      >
+        <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+        <Marker position={userLocation}>
+          <Popup>You are here.</Popup>
+        </Marker>
+      </LeafletMap>
+    );
+  }
+}
 
-export default map;
+export default Map;
