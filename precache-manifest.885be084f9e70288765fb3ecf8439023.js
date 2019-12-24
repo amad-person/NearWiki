943ec665b59a8f11af05f71cf3b18612384f0f29
amad-person <e0072565@u.nus.edu>
2019-12-24 19:45:55 +0530

    Updates

---
+++ precache-manifest.885be084f9e70288765fb3ecf8439023.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b4588f6d5fac5d550a1b5865965161",
+    "url": "/NearWiki/index.html"
+  },
+  {
+    "revision": "7d70492aa2cf5f8f6386",
+    "url": "/NearWiki/static/css/2.447252e7.chunk.css"
+  },
+  {
+    "revision": "7f47e34141286cce9c72",
+    "url": "/NearWiki/static/css/main.b4200ac3.chunk.css"
+  },
+  {
+    "revision": "7d70492aa2cf5f8f6386",
+    "url": "/NearWiki/static/js/2.783b3959.chunk.js"
+  },
+  {
+    "revision": "b8724743c336de7185371245adfc5df1",
+    "url": "/NearWiki/static/js/2.783b3959.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f47e34141286cce9c72",
+    "url": "/NearWiki/static/js/main.b197a33f.chunk.js"
+  },
+  {
+    "revision": "f1be0a7830c46babda81",
+    "url": "/NearWiki/static/js/runtime-main.76c7668e.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/NearWiki/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/NearWiki/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/NearWiki/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/NearWiki/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/NearWiki/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "da01aa0e40fd7acab3245eeba79829b8",
+    "url": "/NearWiki/static/media/confirm.da01aa0e.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/NearWiki/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/NearWiki/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/NearWiki/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/NearWiki/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/NearWiki/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/NearWiki/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "a9bf295f5d2a152c8f1af13447c5d14b",
+    "url": "/NearWiki/static/media/location_search.a9bf295f.svg"
+  },
+  {
+    "revision": "8ccde75d20410ce81f1e8a0c21a8460a",
+    "url": "/NearWiki/static/media/my_location.8ccde75d.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/NearWiki/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/NearWiki/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/NearWiki/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/NearWiki/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/NearWiki/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
